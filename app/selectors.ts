ffdab7ab432607576286b94799dab9d30d5c2c49
Jakub Sowinski <sowinj01@stepstone.com>
2017-02-21 21:09:09 +0000

    Fixed basic setup bugs

--- app/selectors.ts
+++ app/selectors.ts
@@ -1,14 +1,11 @@
 const selectIntl = (state) => {
-  console.log('intl state', state);
   return state.get('intl').toJS();
 };
 
 const selectRouting = (state) => {
-  console.log('routing state', state.get('routing'));
   return {
     locationBeforeTransitions: state.getIn(['routing', 'locationBeforeTransitions'])
   };
-  //return state.get('routing').toJS();
 };
 
 export {
